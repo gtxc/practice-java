3f6f257208eb4f304674e6695b80ffb5c59ce090
gt <gurkanturkarslan@hotmail.com>
2022-03-29 12:30:12 +0300

    Add 2d array serialization info

--- src/com/gtxc/practice/Main.java
+++ src/com/gtxc/practice/Main.java
@@ -16,5 +16,27 @@ public class Main {
         asd.ensureCapacity(1);
         LinkedList<Integer> assd = new LinkedList<>();
         assd.addFirst(1);
+
+
+        int[][] A = {{1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}};
+
+        int rowCount = A.length;
+        int colCount = A[0].length;
+
+        // Diagonal with 2 for, [O(n2)]
+        // for i = 0 : rowCount
+        //     for j = 0 : colCount
+        //         if  i == j
+        //             A[i][j]
+
+        // Diagonal with 1 for (works only for NxN matrices, [O(n)]
+        // for i = 0 : rowCount
+        //     A[i][i]
+
+        // Trace all elements in 2d array with 1 for, [O(n2)]
+        // for i = 0 : rowCount * colCount
+        //     A[i / colCount][i % colCount]
     }
 }
