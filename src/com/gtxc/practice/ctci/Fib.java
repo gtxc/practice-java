11213590c17c7c3e3d3905b24bff1a5fbe40e092
gt <gurkanturkarslan@hotmail.com>
2022-04-01 14:23:53 +0300

    Update Fib.java

--- src/com/gtxc/practice/ctci/Fib.java
+++ src/com/gtxc/practice/ctci/Fib.java
@@ -7,14 +7,35 @@ package com.gtxc.practice.ctci;
 
 public class Fib {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; ++i) {
-            System.out.println(fib(i));
-        }
+        allFib(10);
+        allFibWithMemo(10);
     }
 
     static int fib(int n) {
         if (n <= 0) return 0;
         if (n == 1) return 1;
-        return fib(n-1) + fib(n-2);
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    static void allFib(int n) {
+        for (int i = 0; i < n; ++i) {
+            System.out.println(i + ": " + fib(i));
+        }
+    }
+
+    static int fibWithMemo(int n, int[] memo) {
+        if (n <= 0) return 0;
+        if (n == 1) return 1;
+        if (memo[n] > 0) return memo[n];
+
+        memo[n] = fibWithMemo(n - 1, memo) + fibWithMemo(n - 2, memo);
+        return memo[n];
+    }
+
+    static void allFibWithMemo(int n) {
+        int[] memo = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            System.out.println(i + ": " + fibWithMemo(i, memo));
+        }
     }
 }
