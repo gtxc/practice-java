0feaca1d74cb8f3593a886440cd0542703b21c6a
gt <gurkanturkarslan@hotmail.com>
2022-04-01 12:16:28 +0300

    Add StringPermutation.java

---
+++ src/com/gtxc/practice/StringPermutation.java
@@ -0,0 +1,28 @@
+package com.gtxc.practice;
+
+/*
+    Created by gt at 12:08 PM on Friday, April 01, 2022.
+    Project: practice, Package: com.gtxc.practice.
+*/
+
+public class StringPermutation {
+
+    public static void main(String[] args) {
+        permutation("abc");
+    }
+
+    static void permutation(String str) {
+        permutation(str, "");
+    }
+
+    static void permutation(String str, String prefix) {
+        if (str.length() == 0) {
+            System.out.println(prefix);
+        } else {
+            for (int i = 0; i < str.length(); ++i) {
+                String rem = str.substring(0, i) + str.substring(i+1);
+                permutation(rem, prefix + str.charAt(i));
+            }
+        }
+    }
+}
