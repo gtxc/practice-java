77a9e2d20fd7392141d800922867e765fff12b55
gt <gurkanturkarslan@hotmail.com>
2022-04-18 15:19:11 +0300

    Update emlakjet challenge comments

--- src/com/gtxc/practice/emlakjet/PowersOfTwo.java
+++ src/com/gtxc/practice/emlakjet/PowersOfTwo.java
@@ -6,11 +6,11 @@ package com.gtxc.practice.emlakjet;
 */
 
 /*
-* Have the function PowersOfTwo(num) take the num parameter being passed which will be an integer and return the string
-* true if it's a power of two. If it's not return the string false. For example if the input is 16 then your program
-* should return the string true but if the input is 22 then the output should be the string false.
-*
-*/
+ * Have the function PowersOfTwo(num) take the num parameter being passed which will be an integer and return the string
+ * true if it's a power of two. If it's not return the string false. For example if the input is 16 then your program
+ * should return the string true but if the input is 22 then the output should be the string false.
+ *
+ */
 
 public class PowersOfTwo {
 
